1e3812df59b5b936e6a8e3cb3cf35717e0bd3c36
Filipp Demchenko <filipp5632@gmail.com>
2024-09-06 00:11:15 +0300

    Add /week endpoint to get schedule for current week

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	tele "gopkg.in/telebot.v3"
@@ -30,8 +33,31 @@ func main() {
 		return
 	}
 
+	startDate := time.Now().Add(-time.Duration(time.Now().Weekday()) * time.Hour * 24)
+	endDate := startDate.Add(time.Hour * 24 * 5)
+
+	client := ScheduleClient{Client: http.DefaultClient}
+	provider := NewScheduleProvider(client)
+
 	bot.Handle("/week", func(c tele.Context) error {
-		return c.Send(c.Message().Text)
+		workingDays, err := provider.GetSchedule(startDate, endDate)
+		if err != nil {
+			return c.Send("На жаль відбулася помилка, неможливо отримати розклад.")
+		}
+
+		var message strings.Builder
+		for _, day := range workingDays {
+			message.WriteString(fmt.Sprintf("%s\n", day.DayOfWeekName))
+			for _, lesson := range day.Classes {
+				startTime := lesson.StartTime.Format("15:04")
+				endTime := lesson.EndTime.Format("15:04")
+				message.WriteString(fmt.Sprintf("%s-%s: %s\n", startTime, endTime, lesson.Title))
+				message.WriteString(fmt.Sprintf("%s, %s\n", lesson.Lecturer, lesson.Room))
+			}
+			message.WriteRune('\n')
+		}
+
+		return c.Send(message.String())
 	})
 
 	bot.Start()
