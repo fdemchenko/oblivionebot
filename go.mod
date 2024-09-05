d0a5684d7cdc2ee165017191d7341b6d51e46aaa
Filipp Demchenko <filipp5632@gmail.com>
2024-09-05 17:24:39 +0300

    Create simple echo bot

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fdemchenko/oblivionebot
 
 go 1.23.0
+
+require gopkg.in/telebot.v3 v3.3.8
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"time"
+
+	tele "gopkg.in/telebot.v3"
+)
 
 func main() {
-	fmt.Println("Bot started")
+	botSettings := tele.Settings{
+		Token:  os.Getenv("OBLIVIONE_TG_TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	bot, err := tele.NewBot(botSettings)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	bot.Handle(tele.OnText, func(c tele.Context) error {
+		return c.Send(c.Message().Text)
+	})
+
+	bot.Start()
 }
